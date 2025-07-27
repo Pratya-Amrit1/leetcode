8eb314686cf59787fa3e3d781a8a4cfaa6a00929
pratya amrit <pratya192@gmail.com>
2025-07-27 09:27:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (30.11%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -2,12 +2,12 @@ class Solution {
     public int removeElement(int[] nums, int val) {
        //modify the array in place
         //we will be using two pointer to solve it
-        int k=0;
-        for(int i=0;i< nums.length;i++){
-            if(nums[i] !=val){
-                nums[k++]=nums[i];
-            }
+      int k=0;
+      for(int i=0;i<nums.length ;i++){
+        if(nums[i] !=val){
+            nums[k++]=nums[i];
         }
-        return k;
+      }
+      return k;
     }
 }
