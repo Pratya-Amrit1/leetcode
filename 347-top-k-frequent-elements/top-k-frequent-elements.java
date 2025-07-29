4aa82f1a29dfcb81bbb1b0a8ffc349dc5fb48fd9
pratya amrit <pratya192@gmail.com>
2025-07-29 23:53:34 +0530

    Time: 14 ms (57.53%) | Memory: 49 MB (34.79%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,28 +1,21 @@
 public class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> count = new HashMap<>();
-        List<Integer>[] freq = new List[nums.length + 1];
-
-        for (int i = 0; i < freq.length; i++) {
-            freq[i] = new ArrayList<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
 
-        for (int n : nums) {
-            count.put(n, count.getOrDefault(n, 0) + 1);
-        }
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-            freq[entry.getValue()].add(entry.getKey());
+            heap.offer(new int[]{entry.getValue(), entry.getKey()});
+            if (heap.size() > k) {
+                heap.poll();
+            }
         }
 
         int[] res = new int[k];
-        int index = 0;
-        for (int i = freq.length - 1; i > 0 && index < k; i--) {
-            for (int n : freq[i]) {
-                res[index++] = n;
-                if (index == k) {
-                    return res;
-                }
-            }
+        for (int i = 0; i < k; i++) {
+            res[i] = heap.poll()[1];
         }
         return res;
     }
