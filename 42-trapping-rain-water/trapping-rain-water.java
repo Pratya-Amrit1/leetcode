9b7d988cb9387be9f1b61b035fefa0661d629a4b
pratya amrit <pratya192@gmail.com>
2025-08-28 02:34:23 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (39.25%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,16 +1,32 @@
 class Solution {
     
-    public int trap(int[] h) {  
+    public int trap(int[] arr) { 
+         int l = 0, r = arr.length - 1;
+        int lmax = 0, rmax = 0, ans = 0;
 
-       // by dp
-       
-        int l = 0, r = h.length - 1, lmax = Integer.MIN_VALUE, rmax = Integer.MIN_VALUE, ans = 0;
         while (l < r) {
-        lmax = Math.max(lmax, h[l]);
-        rmax = Math.max(rmax, h[r]);
-        ans += (lmax < rmax) ? lmax - h[l++] : rmax - h[r--];
+            lmax = Math.max(lmax, arr[l]);
+            rmax = Math.max(rmax, arr[r]);
+
+            if (lmax < rmax) {
+                ans += lmax - arr[l];
+                l++;
+            } else {
+                ans += rmax - arr[r];
+                r--;
+            }
         }
-        return ans;
+        return ans; 
+
+    //    // by dp
+       
+    //     int l = 0, r = h.length - 1, lmax = Integer.MIN_VALUE, rmax = Integer.MIN_VALUE, ans = 0;
+    //     while (l < r) {
+    //     lmax = Math.max(lmax, h[l]);
+    //     rmax = Math.max(rmax, h[r]);
+    //     ans += (lmax < rmax) ? lmax - h[l++] : rmax - h[r--];
+    //     }
+    //     return ans;
     
 
 
