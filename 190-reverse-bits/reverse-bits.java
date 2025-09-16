3127ea0d9abd0e621d5f79057afed8cded05b15d
pratya amrit <pratya192@gmail.com>
2025-09-17 02:48:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (79.09%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,11 +1,27 @@
 class Solution {
     public int reverseBits(int n) {
-        int result=0;
-        for( int i=0 ;i<32;i++){
-            result<<=1;
-            result|= (n &1);
-            n>>>=1;
+        
+        // int result=0;
+        // for( int i=0 ;i<32;i++){
+        //     result<<=1;
+        //     result|= (n &1);
+        //     n>>>=1;
+        // }
+        // return result;
+
+  long maxi = 1L << 31;  // 2^31
+        int ans = 0;
+        int count = 0;
+
+        while (count < 32) {
+            if ((n & 1) == 1) {
+                ans += maxi;
+            }
+            n >>= 1;      // shift input right
+            maxi >>= 1;   // shift target position
+            count++;
         }
-        return result;
+
+        return ans;
     }
 }
