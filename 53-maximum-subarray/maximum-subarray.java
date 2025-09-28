1e166d4e8362949fc63b1f249ff218c83c210718
pratya amrit <pratya192@gmail.com>
2025-09-28 22:54:03 +0530

    Time: 1 ms (99.45%) | Memory: 56.9 MB (82.17%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,28 +1,28 @@
 class Solution {
-    // public int maxSubArray(int[] nums) {
-    //     int maxSum = nums[0]; // Initialize with the first element
-    //     int curSum = nums[0];
-
-    //     for (int i = 1; i < nums.length; i++) {
-    //         curSum = Math.max(nums[i], curSum + nums[i]);
-    //         maxSum = Math.max(maxSum, curSum);
-    //     }
-        
-    //     return maxSum;
-    // }
-    
     public int maxSubArray(int[] nums) {
-        int maxSum = nums[0];
-        int currSum = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            currSum += nums[i];
-            maxSum = currSum > maxSum ? currSum : maxSum;
-            if (currSum < 0)
-                currSum = 0;
+        int maxSum = nums[0]; // Initialize with the first element
+        int curSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            curSum = Math.max(nums[i], curSum + nums[i]);
+            maxSum = Math.max(maxSum, curSum);
         }
-        return maxSum;
         
+        return maxSum;
     }
+    
+    // public int maxSubArray(int[] nums) {
+    //     int maxSum = nums[0];
+    //     int currSum = 0;
+    //     int n = nums.length;
+    //     for (int i = 0; i < n; i++) {
+    //         currSum += nums[i];
+    //         maxSum = currSum > maxSum ? currSum : maxSum;
+    //         if (currSum < 0)
+    //             currSum = 0;
+    //     }
+    //     return maxSum;
+        
+    // }
 
 }
