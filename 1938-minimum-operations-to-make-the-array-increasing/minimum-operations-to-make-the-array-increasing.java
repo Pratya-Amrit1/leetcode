8998e3e092ff08a79b417e0963f4d55d6b1518ad
pratya amrit <pratya192@gmail.com>
2025-11-13 01:17:21 +0530

    Time: 4 ms (76.24%) | Memory: 46.9 MB (8.71%) - LeetSync

---
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int operation = 0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]>=nums[i+1])
+            {
+               int need = nums[i]+1 - nums[i+1];
+               operation += need;
+               nums[i+1] = nums[i]+1;
+            }
+        }
+        return operation;
+    }
+}
