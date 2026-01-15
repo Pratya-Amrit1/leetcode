b3aad1dd839bdbeeda53b02322447abdeccf1cd3
pratya amrit <pratya192@gmail.com>
2026-01-15 18:55:05 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (6.33%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -14,21 +14,63 @@ class Node {
 */
 
 class Solution {
+
+    public Node copynodelist(Node head){
+        Node temp =head;
+        while(temp !=null){
+            Node copynode =new Node(temp.val);
+            copynode.next=temp.next;
+            temp.next=copynode;
+            temp=temp.next.next;
+        }
+        return head;
+    }
+    public static Node connran(Node head){
+        Node temp=head;
+        while(temp !=null){
+                Node copynode = temp.next;
+            if(temp.random !=null){
+                copynode.random =temp.random.next;
+            }else{
+                copynode.random =null;
+            }
+                temp=temp.next.next;
+
+        }
+        return head;
+    }
+    public static Node connext(Node head){
+        Node temp =head;
+        Node dummyn = new Node(-1);
+        Node res =dummyn;
+        while(temp !=null){
+            res.next =temp.next;
+            temp.next =temp.next.next;
+            res =res.next;
+            temp=temp.next;
+        }
+        return dummyn.next;
+    }
     public Node copyRandomList(Node head) {
-      Node temp=head;
-      HashMap<Node,Node>mpp= new HashMap<>();
-      while(temp !=null){
-        Node newnode = new Node(temp.val);
-        mpp.put(temp,newnode);
-        temp=temp.next;
-      }
-      temp=head;
-      while(temp !=null){
-         Node copynode= mpp.get(temp);
-        copynode.next = mpp.get(temp.next);
-        copynode.random =mpp.get(temp.random);
-        temp=temp.next;
-      }
-      return mpp.get(head);
+        copynodelist(head);
+        connran(head);
+        return connext(head);
+        
+
+    //   Node temp=head;
+    //   HashMap<Node,Node>mpp= new HashMap<>();
+    //   while(temp !=null){
+    //     Node newnode = new Node(temp.val);
+    //     mpp.put(temp,newnode);
+    //     temp=temp.next;
+    //   }
+    //   temp=head;
+    //   while(temp !=null){
+    //      Node copynode= mpp.get(temp);
+    //     copynode.next = mpp.get(temp.next);
+    //     copynode.random =mpp.get(temp.random);
+    //     temp=temp.next;
+    //   }
+    //   return mpp.get(head);
     }
 }
