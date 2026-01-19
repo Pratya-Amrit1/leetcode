7dd1e809840ddb81fc11a8a0052f8ef4e27759dd
pratya amrit <pratya192@gmail.com>
2026-01-19 17:31:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (18.85%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -20,8 +20,6 @@ class Solution {
             return 0;
         int leftDepth = maxDepth(root.left);
         int rightDepth = maxDepth(root.right);
-
-        
         return Math.max(leftDepth, rightDepth) + 1;
     }
 }
