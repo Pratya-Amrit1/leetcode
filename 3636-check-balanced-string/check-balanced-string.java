6444950979527019545506f12933a5d5a4c52a69
pratya amrit <pratya192@gmail.com>
2026-01-29 11:40:11 +0530

    Time: 2 ms (13.92%) | Memory: 44.8 MB (5.55%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int oddsum=0;
+        int evensum=0;
+       for(int i=0;i<num.length();i++){
+        if( i%2==0){
+            evensum = evensum + Integer.parseInt(String.valueOf(num.charAt(i)));
+        }
+        else{
+            oddsum =oddsum+Integer.parseInt(String.valueOf(num.charAt(i)));
+        }
+
+       }
+       if(oddsum==evensum)return true;
+       return false;
+    }
+}
