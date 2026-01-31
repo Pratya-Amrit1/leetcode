8a3542727d51c709429aef3032ca09c922bd6c78
pratya amrit <pratya192@gmail.com>
2026-01-31 15:32:26 +0530

    Time: 0 ms (100.00%) | Memory: 49.6 MB (39.25%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -13,28 +13,38 @@
  *     }
  * }
  */
-class Solution {
+ class Solution {
     public int countNodes(TreeNode root) {
-        if(root ==null)return 0;
-        int left =gethleft(root);
-        int right =gethright(root);
-        if(left == right)return ((2<<(left))-1);
-        else return countNodes(root.left)+countNodes(root.right)+1;
+        return count(root);
     }
-    public int gethleft(TreeNode root){
-        int count=0;
-        while(root.left !=null){
-            count++;
-            root=root.left;
-        }
-        return count;
-    }
-    public int gethright(TreeNode root){
-        int count=0;
-        while(root.right !=null){
-            count++;
-            root=root.right;
-        }
-        return count;
+
+    public int count(TreeNode n){
+        if(n == null) return 0;
+        return count(n.left) + count(n.right) + 1;
     }
 }
+// class Solution {
+//     public int countNodes(TreeNode root) {
+//         if(root ==null)return 0;
+//         int left =gethleft(root);
+//         int right =gethright(root);
+//         if(left == right)return ((2<<(left))-1);
+//         else return countNodes(root.left)+countNodes(root.right)+1;
+//     }
+//     public int gethleft(TreeNode root){
+//         int count=0;
+//         while(root.left !=null){
+//             count++;
+//             root=root.left;
+//         }
+//         return count;
+//     }
+//     public int gethright(TreeNode root){
+//         int count=0;
+//         while(root.right !=null){
+//             count++;
+//             root=root.right;
+//         }
+//         return count;
+//     }
+// }
