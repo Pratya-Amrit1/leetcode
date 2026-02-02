c5f7ee96bd6de0fef68399667d688199a28e5664
pratya amrit <pratya192@gmail.com>
2026-02-02 19:13:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (90.78%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -15,12 +15,28 @@
  */
 class Solution {
     TreeNode prev =null;
-    public void flatten(TreeNode node) {
-        if(node ==null)return;
-        flatten(node.right);
-        flatten(node.left);
-        node.right =prev;
-        node.left=null;
-        prev=node;
+    public void flatten(TreeNode root) {
+        // if(node ==null)return;
+        // flatten(node.right);
+        // flatten(node.left);
+        // node.right =prev;
+        // node.left=null;
+        // prev=node;
+
+
+        //moris traversal solution
+        TreeNode cur =root;
+        while(cur !=null){
+            if(cur.left !=null){
+            prev=cur.left;
+            while(prev.right !=null){
+                prev=prev.right;
+            }
+            prev.right =cur.right;
+            cur.right =cur.left;
+            cur.left=null;
+            }
+        cur=cur.right;
+        }
     }
 }
