f88c9069a3f25df096c2bb489eefd90926497adf
pratya amrit <pratya192@gmail.com>
2026-02-07 17:31:45 +0530

    Time: 18 ms (100.00%) | Memory: 47.1 MB (99.18%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,22 +1,38 @@
+// class Solution {
+//     public int minimumDeletions(String s) {
+//         int acount=0;
+//         int bcount=0;
+//         int mindel=s.length();
+//         for(char i:s.toCharArray()){
+//             if(i=='a'){
+//                 acount++;
+//             }
+//         }
+//         for(char i:s.toCharArray()){
+//             if(i=='a'){
+//                 acount--;
+//             }
+//             mindel=Math.min(mindel,acount+bcount);
+//              if(i=='b'){
+//                 bcount++;
+//             }
+//         }
+//         return mindel;
+//     }
+// }
+
 class Solution {
     public int minimumDeletions(String s) {
-        int acount=0;
-        int bcount=0;
-        int mindel=s.length();
-        for(char i:s.toCharArray()){
-            if(i=='a'){
-                acount++;
+        int bCount = 0;  
+        int deletions = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == 'b') {
+                bCount++;
+            } else {
+                deletions = Math.min(deletions + 1, bCount);
             }
         }
-        for(char i:s.toCharArray()){
-            if(i=='a'){
-                acount--;
-            }
-            mindel=Math.min(mindel,acount+bcount);
-             if(i=='b'){
-                bcount++;
-            }
-        }
-        return mindel;
+        return deletions;
     }
 }
