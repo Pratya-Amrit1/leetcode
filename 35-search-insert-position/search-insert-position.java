0817c003175eb6464e965b3f3a148a68dba3ae63
pratya amrit <pratya192@gmail.com>
2026-02-23 16:58:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (47.36%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,28 +1,28 @@
-// class Solution {
-//     public int searchInsert(int[] nums, int target) {
-//         int i=0;
-//         while( i< nums.length){
-//             if(target==nums[i]){
-//                 return i;
-//             }
-//             else if(target<nums[i]){
-//                 return i;
-//             }
-//             i++;
-//         }
-//         return i;
-//     }
-// }
-
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int l = 0, r = nums.length-1;
-        while (l <= r){
-            int mid = l + (r - l)/2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) l = mid + 1;
-            else r = mid - 1;
+        int i=0;
+        while( i< nums.length){
+            if(target==nums[i]){
+                return i;
+            }
+            else if(target<nums[i]){
+                return i;
+            }
+            i++;
         }
-        return  l;
+        return i;
     }
 }
+
+// class Solution {
+//     public int searchInsert(int[] nums, int target) {
+//         int l = 0, r = nums.length-1;
+//         while (l <= r){
+//             int mid = l + (r - l)/2;
+//             if (nums[mid] == target) return mid;
+//             else if (nums[mid] < target) l = mid + 1;
+//             else r = mid - 1;
+//         }
+//         return  l;
+//     }
+// }
