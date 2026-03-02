a3c4f50b7688ded3ad1d20128efd406bab2c1319
pratya amrit <pratya192@gmail.com>
2026-03-02 20:16:57 +0530

    Time: 3 ms (85.83%) | Memory: 47.6 MB (70.57%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,17 +1,31 @@
 class Solution {
     public int candy(int[] ratings) {
-        int n = ratings.length;
-        int cnt = 0;
-        int[] candies = new int[n];
-        for (int i = 0; i < n; i++) candies[i] = 1;
-        for (int i = 1; i < n; i++)
-            if (ratings[i] > ratings[i - 1])
-                candies[i] = candies[i - 1] + 1;
-        for (int i = n - 1; i > 0; i--) {
-            if (ratings[i - 1] > ratings[i])
-                candies[i - 1] = Math.max(candies[i] + 1, candies[i - 1]);
-            cnt += candies[i - 1];
+        int sum=1;
+        int i=1;
+        int n= ratings.length;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                i++;
+                sum+=1;
+                continue;
+            }
+            int peak=1;
+            while(i<n &&ratings[i]>ratings[i-1]){
+                peak+=1;
+                sum+=peak;
+                i++;
+            }
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                i++;
+                down++;
+            }
+            if( down>peak)sum+=down-peak;
         }
-        return cnt + candies[n - 1];
+        return sum;
+        
+
+        
     }
 }
