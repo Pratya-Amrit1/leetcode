dc889e771c6dfdea42c34aef43381696071bc80a
pratya amrit <pratya192@gmail.com>
2026-03-04 22:59:23 +0530

    Time: 86 ms (94.59%) | Memory: 136.3 MB (33.65%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -1,49 +1,34 @@
 class Solution {
-    public class Pair {
-        int profit;
-        int cap;
-
-        public Pair(int profit, int cap) {
-            this.profit = profit;
-            this.cap = cap;
-        }
-    }
-
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        int n = profits.length;
-        Pair[] arr = new Pair[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = new Pair(profits[i], capital[i]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> Integer.compare(b,a));
+        IPO[] sortedArr = new IPO[capital.length];
+        for(int i=0; i<profits.length; i++)
+        {
+            sortedArr[i] = new IPO(profits[i],capital[i]);
         }
-
-        Arrays.sort(arr, (a, b) -> {
-            if (a.cap == b.cap) {
-                return b.profit - a.profit;
+        Arrays.sort(sortedArr,(a,b) -> Integer.compare(a.capital,b.capital));
+        int j=0;
+        for(int i=0; i<k; i++)
+        {
+            while(j < profits.length && sortedArr[j].capital <= w)
+            {
+                pq.offer(sortedArr[j].profit);
+                j++;
             }
-            return a.cap - b.cap;
-        });
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>(new Comparator<Pair>() {
-            @Override
-            public int compare(Pair o1, Pair o2) {
-                return o2.profit - o1.profit;
-            }
-        });
-
-        int i = 0;
-        while (i < n && arr[i].cap <= w) {
-            pq.add(new Pair(arr[i].profit, arr[i].cap));
-            i++;
-        }
-        while (!pq.isEmpty() && k != 0) {
-            Pair rv = pq.poll();
-            w += rv.profit;
-            while (i < n && arr[i].cap <= w) {
-                pq.add(new Pair(arr[i].profit, arr[i].cap));
-                i++;
-            }
-            k--;
+            if(pq.isEmpty()) break;
+            w += pq.poll();
         }
         return w;
+    }
+}
+
+public class IPO{
+    int profit;
+    int capital;
+    public IPO(int profit,int capital)
+    {
+        this.profit = profit;
+        this.capital = capital;
     }
 }
