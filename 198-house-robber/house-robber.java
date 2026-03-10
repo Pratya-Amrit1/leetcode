18e7c67b9a2bf04674ab6b1314116cc733dae5bf
pratya amrit <pratya192@gmail.com>
2026-03-10 19:25:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (72.59%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -10,19 +10,33 @@ class Solution {
         return dp[idx]=Math.max(pick,nopick);
     }
     public int rob(int[] nums) {
-        int n= nums.length;
-        int[] dp = new int[n];
-        dp[0]=nums[0];
-        int neg=0;
-        int take=0,notake=0;
-        for(int i=1;i<n;i++){
-            take = nums[i];
-            if(i>1) take+=dp[i-2];
-            notake =0+dp[i-1];
-            dp[i]= Math.max(take,notake);
-                    }
-                    return dp[n-1];
+        // int n= nums.length;
+        // int[] dp = new int[n];
+        // dp[0]=nums[0];
+        // int neg=0;
+        // int take=0,notake=0;
+        // for(int i=1;i<n;i++){
+        //     take = nums[i];
+        //     if(i>1) take+=dp[i-2];
+        //     notake =0+dp[i-1];
+        //     dp[i]= Math.max(take,notake);
+        //             }
+        //             return dp[n-1];
         // Arrays.fill(dp,-1);
         // return f(n-1,nums,dp);
+
+        int n = nums.length;
+        int prev=nums[0];
+        int prev2=0;
+
+        for(int i=1;i<n;i++){
+            int take = nums[i];
+            if(i>1)take+=prev2;
+            int notake = 0+prev;
+            int curr =Math.max(take,notake);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
     }
 }
