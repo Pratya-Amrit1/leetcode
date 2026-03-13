8605ddbc40c8be06924dce0bf0720ca04811a0b0
pratya amrit <pratya192@gmail.com>
2026-03-13 23:35:18 +0530

    Time: 6 ms (100.00%) | Memory: 47.5 MB (96.97%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.java
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.java
@@ -1,23 +1,26 @@
 class Solution {
-    public boolean check(int mountainHeight, int[] workerTimes, long seconds){
-        for(int i=0;i<workerTimes.length;i++){
-            mountainHeight -= (int)((-1 + Math.sqrt(1 + 8L * seconds / workerTimes[i])) / 2);
-            if(mountainHeight<=0) return true;
-        } 
-        return false;
-    }
     public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
-        long low=1;
-        long high=workerTimes[0] * (mountainHeight * (mountainHeight+1L))/2;
-        while(low<high){
-            long mid=low+(high-low)/2;
-            if(check(mountainHeight,workerTimes,mid)){
-                high=mid;
-            }
-            else{
-                low=mid+1;
-            }
+
+        int max = 0;
+        for (int x : workerTimes) 
+            max = Math.max(max, x);
+
+        int h = (mountainHeight-1) / workerTimes.length + 1;
+        long left = 1, right = (long) max * h * (h + 1) / 2;
+        
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            if (check(mountainHeight, workerTimes, mid)) right = mid - 1;
+            else left = mid + 1;
         }
-        return high;
+        return left;
+    }
+
+    boolean check(int mountainHeight, int[] workerTimes, long time) {
+        for (int x : workerTimes) {
+            mountainHeight -= (int)(-1 + Math.sqrt(1 + 8 * time / x)) / 2;
+            if (mountainHeight <= 0) return true;
+        }
+        return false;
     }
 }
