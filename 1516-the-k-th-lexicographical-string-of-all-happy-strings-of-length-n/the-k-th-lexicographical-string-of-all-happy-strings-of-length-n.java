327ba791651a7c417800c2519a2e363b0b568dcc
pratya amrit <pratya192@gmail.com>
2026-03-14 10:47:42 +0530

    Time: 23 ms (18.70%) | Memory: 47.2 MB (10.18%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,36 +1,24 @@
+import java.util.*;
+
 class Solution {
-          ArrayList<String> result=new ArrayList<>();
     public String getHappyString(int n, int k) {
-        if((3*Math.pow(2,n-1))<k){
-            return "";
-        }
-        generate("a",n,k);
-        generate("b",n,k);
-        generate("c",n,k);
-        return result.get(k-1);
+        List<String> list = new ArrayList<>();
+        generate(n, "", list);
+
+        if (k > list.size()) return "";
+        return list.get(k - 1);
     }
-    private void generate(String cur,int n,int k){
-        if(result.size()==k) return ;
-        if(cur.length()==n){
-            result.add(cur);
+
+    private void generate(int n, String curr, List<String> list) {
+        if (curr.length() == n) {
+            list.add(curr);
             return;
         }
-        int l=cur.length();
-       
-        if(cur.charAt(l-1)=='a'){
-            generate(cur+"b",n,k);
-            generate(cur+"c",n,k);   
+
+        for (char c : new char[]{'a','b','c'}) {
+            if (curr.length() == 0 || curr.charAt(curr.length() - 1) != c) {
+                generate(n, curr + c, list);
+            }
         }
-        else if(cur.charAt(l-1)=='b'){
-            generate(cur+"a",n,k);
-            generate(cur+"c",n,k);  
-        }
-        else{
-            generate(cur+"a",n,k);
-            generate(cur+"b",n,k);
-        }
-        
     }
-
-
 }
