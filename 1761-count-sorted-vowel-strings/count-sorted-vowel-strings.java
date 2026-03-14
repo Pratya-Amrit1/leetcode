35020b845b394aaf34ce0cdf226cb5dd3c9ae64e
pratya amrit <pratya192@gmail.com>
2026-03-14 11:00:28 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (67.19%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.java
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.java
@@ -1,5 +1,25 @@
 class Solution {
-    public int countVowelStrings(int n) {
-        return (n+4)*(n+3)*(n+2)*(n+1)/24;
+      public int countVowelStrings(int n) {
+
+        if(n==1) return 5;
+
+        String[] vowels = {"a","e","i","o","u"};
+        int[][] dp = new int[5][n+1];
+
+        for(int i=0;i<5;i++) {
+            dp[i][n] = 1;
+        }
+
+        for(int len=n-1;len>=0;len--) {
+            for(int idx=4;idx>=0;idx--) {
+                if(idx==4) {
+                    dp[idx][len] = dp[idx][len+1];
+                }else {
+                    dp[idx][len] = dp[idx+1][len] + dp[idx][len+1];
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 }
