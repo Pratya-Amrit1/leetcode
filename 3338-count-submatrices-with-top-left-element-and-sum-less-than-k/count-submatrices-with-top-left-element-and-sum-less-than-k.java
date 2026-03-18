3bb7d51912cb6d9e26b04711b81f72f421b5ecaf
pratya amrit <pratya192@gmail.com>
2026-03-18 19:04:41 +0530

    Time: 8 ms (25.81%) | Memory: 168.6 MB (23.39%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -1,36 +1,23 @@
 class Solution {
-    public int countSubmatrices(int[][] grid, int k) {
-        int[][] mem = new int[grid.length][grid[0].length];
-        mem[0][0] = grid[0][0];
-
-        int counter = 0;
-        if (grid[0][0] <= k)
-            counter++;
-
-        for (int i = 1; i < grid.length; ++i) {
-            int sum = mem[i - 1][0] + grid[i][0];
-            mem[i][0] = sum;
-            if (sum <= k)
-                counter++;
+    public int countSubmatrices(int[][] mat, int k) {
+    int n = mat.length;
+    int m = mat[0].length;
+    int[][] pre = new int[n][m];
+    int c = 0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            pre[i][j]=mat[i][j];
+            if(i!=0)   pre[i][j] += pre[i-1][j];
+            if(j!=0)   pre[i][j] += pre[i][j-1];
+            if(i!=0 && j!=0)  pre[i][j] -= pre[i-1][j-1];
+            if(pre[i][j]<=k)
+                c++;
+            else
+                m=j;
         }
-
-        for (int j = 1; j < grid[0].length; ++j) {
-            int sum = mem[0][j - 1] + grid[0][j];
-            mem[0][j] = sum;
-            if (sum <= k)
-                counter++;
-        }
-
-        for (int i = 1; i < grid.length; ++i) {
-            for (int j = 1; j < grid[0].length; ++j) {
-                int sum = mem[i][j - 1] + (mem[i - 1][j] - mem[i - 1][j - 1]) + grid[i][j];
-                mem[i][j] = sum;
-                if (sum <= k)
-                    counter++;
-            }
-        }
-
-        return counter;
-
+    }
+    return c;
     }
 }
