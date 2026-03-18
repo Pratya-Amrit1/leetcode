ef0ce34c3d4220805c4e54f7a38366a04fc6b072
pratya amrit <pratya192@gmail.com>
2026-03-18 19:04:02 +0530

    Time: 4 ms (95.16%) | Memory: 168.5 MB (26.61%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -1,17 +1,36 @@
 class Solution {
     public int countSubmatrices(int[][] grid, int k) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int prefixSum[] = new int[n];
-        int ans = 0;
-        for(int i=0;i<m;i++){
-            int currSum = 0;
-            for(int j=0;j<n;j++){
-                prefixSum[j] = prefixSum[j]+grid[i][j];
-                currSum += prefixSum[j];
-                if(currSum <= k)ans++;
+        int[][] mem = new int[grid.length][grid[0].length];
+        mem[0][0] = grid[0][0];
+
+        int counter = 0;
+        if (grid[0][0] <= k)
+            counter++;
+
+        for (int i = 1; i < grid.length; ++i) {
+            int sum = mem[i - 1][0] + grid[i][0];
+            mem[i][0] = sum;
+            if (sum <= k)
+                counter++;
+        }
+
+        for (int j = 1; j < grid[0].length; ++j) {
+            int sum = mem[0][j - 1] + grid[0][j];
+            mem[0][j] = sum;
+            if (sum <= k)
+                counter++;
+        }
+
+        for (int i = 1; i < grid.length; ++i) {
+            for (int j = 1; j < grid[0].length; ++j) {
+                int sum = mem[i][j - 1] + (mem[i - 1][j] - mem[i - 1][j - 1]) + grid[i][j];
+                mem[i][j] = sum;
+                if (sum <= k)
+                    counter++;
             }
         }
-        return ans;
+
+        return counter;
+
     }
 }
