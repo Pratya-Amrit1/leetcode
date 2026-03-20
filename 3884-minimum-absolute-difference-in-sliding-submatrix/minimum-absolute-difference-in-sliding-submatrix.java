dfd19ffe1f5e4acab790d885d1538aa167752701
pratya amrit <pratya192@gmail.com>
2026-03-20 21:39:39 +0530

    Time: 12 ms (80.85%) | Memory: 47.8 MB (40.43%) - LeetSync

--- 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
@@ -1,36 +1,35 @@
 class Solution {
-    public int getmn(int[] arr){
-        Arrays.sort(arr);
-        int mn = Integer.MAX_VALUE;
-        for(int i = 1;i< arr.length;i++){
-            if(arr[i - 1] != arr[i]){
-                mn = Math.min(mn, Math.abs(arr[i] - arr[i - 1]));
+    int getMin(int[][] g,int k,int x,int y){
+        int m = g.length,n = g[0].length;
+        int[] temp = new int[k*k];
+        int st = 0;
+        //linearize the submatrix 
+        for(int i = 0;i<k;i++){
+            for(int j = 0;j<k;j++){
+                temp[st++] = g[i+x][j+y];
             }
-            
         }
-        return mn == Integer.MAX_VALUE? 0:mn;
+        //sort the linear array
+        Arrays.sort(temp);
+        int diff = Integer.MAX_VALUE;
+        for(int i = 1;i<k*k;i++){
+            //skip the same elements
+            while(i<k*k && temp[i]==temp[i-1]) i++;
+            if(i<k*k)  diff = Math.min(diff,Math.abs(temp[i]-temp[i-1]));
+        }
+        //return min diff
+        return diff;
     }
     public int[][] minAbsDiff(int[][] grid, int k) {
-        // 1 1 1
-        // 1 1 1
-        // 1 1 1
-        int m = grid.length, n = grid[0].length;
-        int[][] answ = new int[m - k + 1][n - k + 1];
-        for(int row = 0; row < m - k + 1;row++){
-            for(int col = 0; col < n - k + 1;col++){
-                int[] arr = new int[k * k];
-                int idx = 0;
-                for(int i = row;i < row + k;i++){
-                    for(int j = col;j < col + k;j++){
-                        arr[idx] = grid[i][j];
-                        idx++;
-                    }
-                }
-                answ[row][col] = getmn(arr);
-                
-
+        int m = grid.length,n = grid[0].length;
+        int[][] ans = new int[m-k+1][n-k+1];
+        //generate all submatrix
+        for(int i = 0;i<=m-k;i++){
+            for(int j = 0;j<=n-k;j++){
+                int temp = getMin(grid,k,i,j);
+                ans[i][j] = temp==Integer.MAX_VALUE?0:temp;
             }
         }
-        return answ;
+        return ans;
     }
 }
