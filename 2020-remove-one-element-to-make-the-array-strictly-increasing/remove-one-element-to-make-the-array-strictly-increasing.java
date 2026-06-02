f9fcfa11d9dbd5da482daced7d5c9fe27b0842d5
pratya amrit <pratya192@gmail.com>
2026-06-02 16:00:42 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (63.46%) - LeetSync

---
+++ 2020-remove-one-element-to-make-the-array-strictly-increasing/remove-one-element-to-make-the-array-strictly-increasing.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canBeIncreasing(int[] nums) {
+        int len=nums.length;
+        int cnt=0;
+        for(int i=1;i<len;i++){
+            if(nums[i]<=nums[i-1]){
+                if(nums[i]<=nums[i-1]){
+                   if(++cnt>1){
+                    return false;
+                   } 
+                   if((i>1 && nums[i] <=nums[i-2])&&(i+1<len && nums[i+1] <=nums[i-1])){
+                    return false;
+                   }
+                }
+            }
+        }
+        return true;
+    }
+}
