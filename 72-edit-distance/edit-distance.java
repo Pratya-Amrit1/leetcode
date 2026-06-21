75235f27212d816f092f493b37ecdbd3d443cb1d
pratya amrit <pratya192@gmail.com>
2026-06-21 18:48:53 +0530

    Time: 5 ms (69.00%) | Memory: 47.1 MB (52.27%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -13,10 +13,26 @@ class Solution {
     public int minDistance(String s, String t) {
         int n=s.length();
         int m=t.length();
-        int[][] dp= new int [n][m];
+        int[][] dp= new int [n+1][m+1];
         for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+            Arrays.fill(dp[i],0);
         }
-        return solve(n-1,m-1,s,t,dp);
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1+Math.min(dp[i-1][j], Math.min(dp[i-1][j-1],dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
