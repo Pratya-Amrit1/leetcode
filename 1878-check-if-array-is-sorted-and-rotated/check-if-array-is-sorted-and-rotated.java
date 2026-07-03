c198680f286c8434d71f29d6084f4f933651eb8f
pratya amrit <pratya192@gmail.com>
2026-07-03 18:44:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (11.21%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,13 +1,17 @@
 class Solution {
     public boolean check(int[] nums) {
-        int count =0;
-        int n= nums.length;
-        for(int i=0 ; i<n;i++){
-            if(nums[i]> nums[(i+1)%n]){ //modulo is used to find the  small no and the s
-
-            ++count;
+        int breaki=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]){
+                breaki++;
             }
         }
-        return (count<=1);
+            if(nums[0]<nums[nums.length-1]){
+                breaki++;
+            }
+        if( breaki>1){
+            return false;
+        }
+        return true;
     }
 }
