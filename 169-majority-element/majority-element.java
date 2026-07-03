92a52b9ea3d496840579fe41e4dd100f9a076829
pratya amrit <pratya192@gmail.com>
2026-07-03 19:21:35 +0530

    Time: 12 ms (35.91%) | Memory: 52.8 MB (76.43%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,24 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-    //     HashMap<Integer , Integer> mpp= new HashMap<>();
-    //     for(int i=0;i<nums.length;i++){
-    //         mpp.put(nums[i] ,mpp.getOrDefault(nums[i],0)+1 );
-    //         if(mpp.get(nums[i]) >nums.length/2){
-    //             return nums[i];
-    //         }
-    //     }
-    //     return -1;
-    // }
-    //boyer moryee model
-
-    int count=0 , candidate=0;
-
-    for(int num :nums){
-        if(count==0){
-        candidate=num;
+        int n=nums.length;
+        int a=-1;
+        Map<Integer,Integer> mp =new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry:mp.entrySet()){
+            if(entry.getValue()>n/2){
+                 a= entry.getKey();
+            }
+        }
+        return a;
     }
-    count+=(num == candidate)? 1 :-1;
-    
 }
-return candidate;
-        } }
