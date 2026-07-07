10da64dad33147a3b0cbfb38a85fe943aea3bc10
pratya amrit <pratya192@gmail.com>
2026-07-07 12:14:05 +0530

    Time: 1 ms (94.05%) | Memory: 43.5 MB (33.39%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,15 +1,28 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-         List<List<Integer>> pas = new ArrayList<>();
+        List<List<Integer>> triangle = new ArrayList<>();
+        
         for (int i = 0; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>(Collections.nCopies(i + 1, 1));
-            
-            for (int j = 1; j < i; j++) {
-                int val = pas.get(i - 1).get(j - 1) + pas.get(i - 1).get(j);
-                row.set(j, val);
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                // Every cell in Pascal's triangle is exactly nCr!
+                row.add(nCr(i, j));
             }
-            pas.add(row);
+            triangle.add(row);
         }
-        return pas;
+        return triangle;
+    }
+    
+    // Helper function to calculate combinations (n choose r)
+    private int nCr(int n, int r) {
+        long res = 1;
+        // Optimize by taking the smaller half
+        if (r > n - r) r = n - r; 
+        
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return (int) res;
     }
 }
