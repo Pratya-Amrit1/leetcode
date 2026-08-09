52b99e692fb100e91f75aa17981b9a849c057a4b
pratya amrit <pratya192@gmail.com>
2026-08-09 19:22:17 +0530

    Time: 1 ms (99.72%) | Memory: 45.5 MB (7.81%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -13,40 +13,40 @@
  *     }
  * }
  */
- class pair{
+ class Pair{
     TreeNode node;
-    int num;
-    pair(TreeNode node ,int num){
+    int ind;
+    Pair(TreeNode node,int ind){
         this.node =node;
-        this.num =num;
+        this.ind=ind;
     }
  }
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if(root ==null)return 0;
+        if(root==null)return 0;
         int ans=0;
-        Queue<pair> q =new LinkedList<>();
-        q.offer(new pair(root ,0));
-        while(! q.isEmpty()){
-            int size =q.size();
-            int mmin =q.peek().num;
+        Queue<Pair> q=new LinkedList<>();
+        q.offer(new Pair(root,0));
+        while(!q.isEmpty()){
+            int size=q.size();
+            int mini=q.peek().ind;
             int first=0;
-            int last =0;
+            int last=0;
             for(int i=0;i<size;i++){
-                int curr =q.peek().num-mmin;
+                int curind= q.peek().ind-mini;
                 TreeNode node =q.peek().node;
                 q.poll();
-                if(i==0) first =curr;
-                if(i==size-1) last=curr;
-                if(node.left !=null){
-                    q.offer(new pair(node.left ,curr*2+1));
+                if(i==0)first=curind;
+                if(i==size-1)last=curind;
+                if(node.left!=null){
+                    q.offer(new Pair(node.left,curind*2+1));
                 }
-                if(node.right !=null){
-                    q.offer(new pair(node.right ,curr*2+2));
+                if(node.right!=null){
+                    q.offer(new Pair(node.right,curind*2+2));
                 }
-                ans = Math.max(ans ,last-first+1);
             }
+            ans=Math.max(ans,last-first+1);
         }
-            return ans;
+        return ans;
     }
 }
