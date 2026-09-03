d21c2fe7bbf3753956d23c8738eb29dd0f04a872
pratya amrit <pratya192@gmail.com>
2026-09-03 10:02:21 +0530

    Time: 3 ms (88.13%) | Memory: 52.2 MB (58.32%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,22 +1,49 @@
 class Solution {
+
     public int numIslands(char[][] grid) {
-    int count=0;
-    for(int i=0;i<grid.length;i++)
-        for(int j=0;j<grid[0].length;j++){
-            if(grid[i][j]=='1'){
-                dfsFill(grid,i,j);
-                count++;
+
+        int count = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+
+            for (int j = 0; j < grid[0].length; j++) {
+
+                // Found a new island
+                if (grid[i][j] == '1') {
+
+                    count++;
+
+                    // Visit the complete island
+                    dfs(grid, i, j);
+                }
             }
         }
-    return count;
-}
-private void dfsFill(char[][] grid,int i, int j){
-    if(i>=0 && j>=0 && i<grid.length && j<grid[0].length&&grid[i][j]=='1'){
-        grid[i][j]='0';
-        dfsFill(grid, i + 1, j);
-        dfsFill(grid, i - 1, j);
-        dfsFill(grid, i, j + 1);
-        dfsFill(grid, i, j - 1);
+
+        return count;
     }
-}
+
+    private void dfs(char[][] grid, int row, int col) {
+
+        // Boundary check
+        if (row < 0 || row >= grid.length ||
+            col < 0 || col >= grid[0].length ||
+            grid[row][col] == '0') {
+            return;
+        }
+
+        // Mark land as visited
+        grid[row][col] = '0';
+
+        // Visit up
+        dfs(grid, row - 1, col);
+
+        // Visit down
+        dfs(grid, row + 1, col);
+
+        // Visit left
+        dfs(grid, row, col - 1);
+
+        // Visit right
+        dfs(grid, row, col + 1);
+    }
 }
